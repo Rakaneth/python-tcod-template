eef54b0ab94c9c5bf13d19f819cacd78a54961cc
Quincy West <rakaneth@gmail.com>
2022-10-08 01:32:46 -0400

    add tiles scaffolding

--- screens/test.py
+++ screens/test.py
@@ -2,6 +2,7 @@ import tcod
 
 from typing import Optional
 from screen import Screen, HandleResult
+from tiles import TILE_WALL, TILE_FLOOR
 
 class TestScreen(Screen):
     """A test screen."""
@@ -35,6 +36,8 @@ class PlayScreen(Screen):
     
     def render(self, root_con: tcod.Console):
         root_con.print(0, 0, "Press [w] to win and [l] to lose")
+        root_con.rgb[1, 1] = TILE_WALL["dark"]
+        root_con.rgb[1, 2] = TILE_FLOOR["dark"]
     
     def on_key(self, sym: tcod.event.KeySym) -> Optional[HandleResult]:
         new_screen = None
